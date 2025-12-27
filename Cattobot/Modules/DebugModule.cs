68f4ba54eed080f538fbd0d5ba0fb8788d570c0c
lanternmouse <thesaratys@gmail.com>
2025-12-27 19:40:40 +0500

    fix

--- Cattobot/Modules/DebugModule.cs
+++ Cattobot/Modules/DebugModule.cs
@@ -1,12 +1,15 @@
 using Discord.Interactions;
+using Discord.WebSocket;
 
 namespace Cattobot.Modules;
 
-public class DebugModule : InteractionModuleBase
+public class DebugModule(
+    DiscordSocketClient client
+    ) : InteractionModuleBase
 {
     [SlashCommand("ping", "Check availability")]
     public async Task Ping()
     {
-        await RespondAsync("Pong!");
+        await RespondAsync($"Pong! Latency is {client.Latency} ms");
     }
 }
