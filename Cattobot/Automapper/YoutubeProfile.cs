149f2b4177ee017b7ad4333af7e639ca300eeeef
lanternmouse <thesaratys@gmail.com>
2026-01-18 00:52:31 +0500

    delete obsolete code

--- Cattobot/Automapper/YoutubeProfile.cs
+++ Cattobot/Automapper/YoutubeProfile.cs
@@ -1,25 +1,13 @@
-using System.Globalization;
 using Cattobot.Db.Models;
 using Cattobot.Youtube.Gateway.Models;
 using Mapster;
-using Nager.Country.Translation;
-using TMDbLib.Objects.Movies;
 
 namespace Cattobot.Automapper;
 
-public class YtDlpProfile : IRegister
+public class YoutubeProfile : IRegister
 {
     public void Register(TypeAdapterConfig config)
     {
-        config.NewConfig<FullMediaInfo, TrackDb>()
-            .Map(d => d.Id, s => Guid.Empty)
-            .Map(d => d.Title, s => s.Title)
-            .Map(d => d.AddedOn, s => DateTime.UtcNow)
-            .Map(d => d.Artist, s => s.Channel)
-            .Map(d => d.Duration, s => s.Duration)
-            .Map(d => d.ThumbnailUrl, s => s.Thumbnail)
-            .Map(d => d.ExternalUrl, s => s.WebpageUrl);
-
         config.NewConfig<YoutubeVideoInfo.Root, TrackDb>()
             .Map(d => d.Id, s => Guid.Empty)
             .Map(d => d.Title, s => s.VideoDetails.Title)
--- Kinopoisk.Gateway/Configuration/KinopoiskOptions.cs
+++
@@ -1,9 +0,0 @@
-using System.ComponentModel.DataAnnotations;
-
-namespace Kinopoisk.Gateway.Configuration;
-
-public class KinopoiskOptions
-{
-    [Required] public string Url { get; set; }
-    [Required] public string Token { get; set; }
-}
